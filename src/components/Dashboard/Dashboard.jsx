4f07a0759c33aaec12d948c26ddbd5f069f85f4e
Iasi Salomon <iasisalomon@gmail.com>
2022-10-13 22:26:36 -0300

    some changes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,33 +1,97 @@
-// import React, { useEffect, useState } from "react";
-// import { CartState } from "../../context/Context";
-// import datas from "../../data/product_data.json";
-import dataBorrows from "../../data/borrow_data.json";
-import dataLens from "../../data/lend_data.json";
-import CreateOrder from "../Partials/CreateOrder";
-import Layout from "../Partials/Layout";
-import TxHistory from "../Partials/TxHistory";
-import MyBorrowStatus from "./MyBorrowStatus";
-import MyLendStatus from "./MyLendStatus";
-// import MyLoanStatus from "./MyLoanStatus";
+import React, { useState } from 'react';
+import dataLens from '../../data/lend_data.json';
+import CreateOrder from '../Partials/CreateOrder';
+import Layout from '../Partials/Layout';
+import TxHistory from '../Partials/TxHistory';
+import MyLendStatus from './MyLendStatus';
 
+export default function Nubicuo() {
+	const allLends = dataLens.data;
 
-export default function Nubicuo () {
-  // const allBids = datas.datas;
-  const allLends = dataLens.data;
-  const allBorrows = dataBorrows.data;
+	const [selectedButton, setSelectedButton] = useState('Assets');
+	const [selectedOrder, setSelectedOrder] = useState(allLends);
 
-  return (
-    <>
-      <Layout>
+	function changeTabHandler(buttonValue) {
+		setSelectedButton(buttonValue);
+		for (let i = 0; i < allLends.length; i++) {
+			const number = `${Math.floor(Math.random() * 999)},${Math.floor(Math.random() * 999)} ETH`;
+			console.log(number);
+			allLends[i].price = number;
+			setSelectedOrder(allLends);
+		}
+	}
 
-        <div className="home-page-wrapper">
-          <CreateOrder />
-        </div>
-        <MyLendStatus allLends={allLends} className="mb-10" />
-        <MyBorrowStatus allBorrows={allBorrows} className="mb-10" />
-        
-        <TxHistory className="mb-10" />
-      </Layout>
-    </>
-  );
+	return (
+		<>
+			<Layout>
+				<div className='home-page-wrapper'>
+					<CreateOrder />
+					<div className='m-2 grid grid-cols-3 justify-center'>
+						<button
+							onClick={() => changeTabHandler('Assets')}
+							type='button'
+							style={{
+								backgroundColor: selectedButton === 'Assets' ? '#ddd' : '#333',
+								outline: '2px solid #333',
+							}}
+							className='flex cursor-pointer lg:w-[250px] w-[150px] h-[40px] items-center rounded-full pr-4 pl-4'
+						>
+							<div className='flex items-center justify-between w-full h-full'>
+								<span className='lg:block hidden'></span>
+								<p
+									className='lg:text-m text-m font-bold text-white'
+									style={{ color: selectedButton === 'Assets' ? '#333' : '#ccc' }}
+								>
+									My Assets
+								</p>
+								<span className='lg:block hidden'></span>
+							</div>
+						</button>
+						<button
+							onClick={() => changeTabHandler('Loans')}
+							type='button'
+							style={{
+								backgroundColor: selectedButton === 'Loans' ? '#ddd' : '#333',
+								outline: '2px solid #333',
+							}}
+							className='flex cursor-pointer lg:w-[250px] w-[150px] h-[40px] items-center rounded-full pr-4 pl-4'
+						>
+							<div className='flex items-center justify-between w-full h-full'>
+								<span className='lg:block hidden'></span>
+								<p
+									className='lg:text-m text-m font-bold text-white'
+									style={{ color: selectedButton === 'Loans' ? '#333' : '#ccc' }}
+								>
+									My Loans
+								</p>
+								<span className='lg:block hidden'></span>
+							</div>
+						</button>
+						<button
+							onClick={() => changeTabHandler('Swaps')}
+							type='button'
+							style={{
+								backgroundColor: selectedButton === 'Swaps' ? '#ddd' : '#333',
+								outline: '2px solid #333',
+							}}
+							className='flex cursor-pointer lg:w-[250px] w-[150px] h-[40px] items-center rounded-full pr-4 pl-4'
+						>
+							<div className='flex items-center justify-between w-full h-full'>
+								<span className='lg:block hidden'></span>
+								<p
+									className='lg:text-m text-m font-bold text-white'
+									style={{ color: selectedButton === 'Swaps' ? '#333' : '#ccc' }}
+								>
+									My Swaps
+								</p>
+								<span className='lg:block hidden'></span>
+							</div>
+						</button>
+					</div>
+				</div>
+				<MyLendStatus allLends={selectedOrder} className='mt-5 mb-10' />
+				<TxHistory className='mb-10' />
+			</Layout>
+		</>
+	);
 }
---
+++ src/components/Dashboard/ThreeButtons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DataIterationItem() {
+	return (
+		<>
+			<div className='item'>{}</div>
+		</>
+	);
+}
+
+export default DataIterationItem;
